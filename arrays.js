8e0c4f72e2bd59ec040cfef3c3e0be655cd37f37
dabobmarley@hotmail.com <dabobmarley@hotmail.com>
2018-05-17 13:09:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,4 +8,8 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   return array.unshift(element);
 }
 
+function addElementToEndOfArray(array, element){
+  return [...array, element];
+}
+
   
